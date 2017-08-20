a1999ab423fe82fd849e1092cc2a52f000a49a4a
S K Manna <sk.manna@gmail.com>
2017-08-21 00:03:55 +0530

    Add /POST users route, and token and cutomo toJSON methods in user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,49 @@
 const { mongoose } = require('../db/mongoose');
-
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
+        unique: true,
         trim: true,
-        minLength: 1
-    }
+        minlength: 1,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true,
+        },
+        token: {
+            type: String,
+            required: true,
+        }
+    }]
 });
 
+userSchema.methods.toJSON = function() {
+    const user = this;
+    const userObject = user.toObject();
+    return _.pick(userObject, ['_id', 'email']);
+}
+userSchema.methods.generateAuthToken = function() {
+    let user = this;
+    const access = 'auth';
+    const token = jwt.sign({_id: user._id.toHexString(), access }, 'abc123').toString();
+    user.tokens.push({access, token});
+    return user.save().then(() => {
+            return token;
+        });
+}
 module.exports = mongoose.model('User', userSchema);
--- server/server.js
+++ server/server.js
@@ -92,6 +92,22 @@ app.patch('/todos/:id', (req, res) => {
             res.status(404).send();
         })
 });
+
+//User routes
+
+app.post('/users', (req, res) => {
+    const body = _.pick(req.body, ['email', 'password']);
+    const user = new User(body);
+    user.save()
+        .then(() => {
+            // res.send(user)
+            return user.generateAuthToken();
+        }).then((token) => {
+            res.header('x-auth', token).send(user);    
+        }).catch((err) => res.status(400).send(err))
+});
+
+
 app.listen(process.env.PORT, () => {
     console.log(`App started on port ${process.env.PORT}`);
 });
