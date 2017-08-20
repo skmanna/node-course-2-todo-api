9a67e3772536c020665bc69aca0b72d926cc76eb
S K Manna <sk.manna@gmail.com>
2017-08-20 13:09:09 +0530

    Prepare for Heroku deployment

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ const User = require('./models/user');
 
 let app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -48,8 +50,8 @@ app.get('/todos/:id', (req, res) => {
             res.status(400).send()
         });
 })
-app.listen(3000, () => {
-    console.log(`App started on port 3000`);
+app.listen(port, () => {
+    console.log(`App started on port ${port}`);
 });
 
 module.exports = app;
